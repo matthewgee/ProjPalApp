cbf8b03aca097bb46afd0dad4de95dfdea39af66
Matthew Gee <mattgee@gmail.com>
2012-08-08 15:13:01 -0500

    Initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ProjPalApp::Application.config.session_store :cookie_store, key: '_ProjPalApp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ProjPalApp::Application.config.session_store :active_record_store
