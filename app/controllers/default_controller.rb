f679c5e3839341d97cb028da65c270a9ad2f2c04
Duane Johnson <duane.johnson@gmail.com>
2012-08-16 09:31:30 -0500

    Added raw dump of github events to default view

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -3,9 +3,8 @@ require "httparty"
 class DefaultController < ApplicationController
   def home
     if access_token
-      @profiles = HTTParty.get(profiles_url,
-        :query => {:access_token => access_token}
-      ).parsed_response
+      @profiles = get_singly(profiles_url)
+      @github_events = get_singly(github_events_url)
     end
   end
 
@@ -20,4 +19,15 @@ private
   def profiles_url
     "#{SINGLY_API_BASE}/profiles"
   end
+
+  def github_events_url
+    "#{SINGLY_API_BASE}/v0/services/github/events"
+  end
+
+  def get_singly(url)
+    HTTParty.get(url,
+        :query => {:access_token => access_token}
+      ).parsed_response
+  end
+
 end
