8c53b67612474710b0c388aeff4b3afe1ab9d653
Duane Johnson <duane.johnson@gmail.com>
2012-08-24 08:42:41 -0500

    Rearrange attributes in Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ActiveRecord::Base
-  attr_accessible :description, :id, :name, :person_id, :project_id, :shared_event_id, :source, :type, :visibility, :when_occurred
+  attr_accessible :id
+  attr_accessible :person_id, :project_id, :shared_event_id
+  attr_accessible :name, :description
+  attr_accessible :source, :type, :visibility, :when_occurred
 end
