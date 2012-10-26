9e2272884023a7febe4305009ce608dfa1cbbbe9
Duane Johnson <duane.johnson@gmail.com>
2012-10-26 07:11:27 -0600

    Add person factory

---
+++ spec/factories/person.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :person do
+    firstname "John"
+    sequence(:lastname) { |n| "Doe-#{n}" }
+    sequence(:email) {|n| "person#{n}@example.com" }
+    bio { generate(:random_string) }
+  end
+end
