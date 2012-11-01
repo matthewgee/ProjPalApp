d2e52b97c0ec0e911cc904a41cf16797c320f8b0
Duane Johnson <duane.johnson@gmail.com>
2012-11-01 07:08:54 -0600

    Include rails env in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,12 @@
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
 require 'mongoid'
 require 'factory_girl_rails'
-ENV["RAILS_ENV"] = 'test'
+
+ENV["RAILS_ENV"] ||= 'test'
 
 RSpec.configure do |config|
+  
   config.mock_with :rspec
 
   config.include Mongoid::Matchers
